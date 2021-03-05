1a25b64e83d41fb3ce2eb6d9f620b25284cd5fb5
tanmayjagtap <tanmayjagtap27@gmail.com>
2021-03-05 23:00:18 +0530

    Home Carousel Fixed

--- src/Components/BackgroundCarousel/BackgroundCarousel.jsx
+++ src/Components/BackgroundCarousel/BackgroundCarousel.jsx
@@ -5,8 +5,8 @@ import Logo from './Logo.svg';
 
 const BackgroundCarousel = () => {
     return(
-        <div style={{marginLeft:'-10px'}}>
-            <img src={Logo} style={{height:'100px',width:'100px'}} className='center-logo'/>
+        <div style={{marginLeft:'-10px',marginRight:'-10px'}}>
+            <img src={Logo} style={{height:'100px',width:'100px'}} className='center-logo' alt='Logo'/>
             <aside className="left-side side-nav">
         <ul className="mr-auto ml-auto">
           <li className="di pl3 grow">
@@ -39,6 +39,54 @@ const BackgroundCarousel = () => {
         </ul>
       </aside>
             <Carousel enableAutoPlay='true' autoPlaySpeed='2000'>
+                <div className='slide1 flex justify-center items-center font-size flex-column'>
+                    <div className='red b tl'>
+                        CREATE
+                    </div>
+                    <div className='b white stroke tl'>
+                        WEALTH
+                    </div>
+                </div>
+                <div className='slide2 flex justify-center items-center font-size flex-column'>
+                    <div className='red b tl'>
+                        ACCURATE
+                    </div>
+                    <div className='b white stroke tl'>
+                        SIGNALS
+                    </div>
+                </div>
+                <div className='slide3 flex justify-center items-center font-size flex-column'>
+                    <div className='red b tl'>
+                        EXPERTISED
+                    </div>
+                    <div className='b white stroke tl'>
+                        TEACHING
+                    </div>
+                </div>
+                <div className='slide1 flex justify-center items-center font-size flex-column'>
+                    <div className='red b tl'>
+                        CREATE
+                    </div>
+                    <div className='b white stroke tl'>
+                        WEALTH
+                    </div>
+                </div>
+                <div className='slide2 flex justify-center items-center font-size flex-column'>
+                    <div className='red b tl'>
+                        ACCURATE
+                    </div>
+                    <div className='b white stroke tl'>
+                        SIGNALS
+                    </div>
+                </div>
+                <div className='slide3 flex justify-center items-center font-size flex-column'>
+                    <div className='red b tl'>
+                        EXPERTISED
+                    </div>
+                    <div className='b white stroke tl'>
+                        TEACHING
+                    </div>
+                </div>
                 <div className='slide1 flex justify-center items-center font-size flex-column'>
                     <div className='red b tl'>
                         CREATE
