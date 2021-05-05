43509cac11b8b102a5e78c0eeccb1e4c1c1c9fba
sohamkhade <soham9623392426@gmail.com>
2021-05-05 21:46:06 +0530

    scroll removed

--- src/Components/StillConfusing/StillConfusing.js
+++ src/Components/StillConfusing/StillConfusing.js
@@ -9,11 +9,11 @@ export default function stillConfusing() {
         <section className={'mr-auto bg-dark-blue pv6-ns tl'}>
             <div className={'pv5 pb5 w-70-ns ml-auto-ns mr-auto-ns white scroll-none-x'}>
                 <div className={'pl3'}>
-                    <p data-aos="fade-left" data-aos-duration={"1000"} className={'pt1 mb0-ns downmove scroll-none-x'}>START LEARNING WITH US</p>
-                    <h1 data-aos="fade-left"  data-aos-duration={"1000"} className={'f1-ns scroll-none-x'}>Still <span className={'red'}>Confused ?</span></h1>
+                    <p data-aos="fade-left" data-aos-duration={"1000"} style={{overflow: "hidden"}} className={'pt1 mb0-ns downmove scroll-none-x'}>START LEARNING WITH US</p>
+                    <h1 data-aos="fade-left"  data-aos-duration={"1000"} style={{overflow: "hidden"}} className={'f1-ns scroll-none-x'}>Still <span className={'red'}>Confused ?</span></h1>
                 </div>
-                <div className={'w-60-ns pl3 pr3 mr2 scroll-none-x'}>
-                    <p data-aos="fade-left" data-aos-delay="100" data-aos-duration="1500" className={'scroll-none-x w-80-ns pb3-ns f4-ns'}>We understand your problems. You may have many questions boiling up in your mind. To address your
+                <div className={'w-60-ns pl3 pr3 mr2 scroll-none-x'} style={{overflow: "hidden"}}>
+                    <p data-aos="fade-left" data-aos-delay="100" data-aos-duration="1500" style={{overflow: "hidden"}} className={'scroll-none-x w-80-ns pb3-ns f4-ns'}>We understand your problems. You may have many questions boiling up in your mind. To address your
                         concerns we recommend you sign up for a Premium consultation with us.</p>
                     <a data-aos="fade-up" data-aos-duration="1000"href={'https://instagram.com/cyberking_capitals?igshid=mjv77zi1djho'} className={'f2'}><FiInstagram color={'white'} /></a>
                     <a data-aos="fade-up" data-aos-delay={"100"} data-aos-duration="1500" href={''} className={'f2 pl2'}><AiOutlineMail color={'white'}/></a>
